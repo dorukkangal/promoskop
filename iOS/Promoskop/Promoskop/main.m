5c19fd3e303ac8e5d9e6ae7d2dd84fe3cc4313f5
Mustafa Besnili <mustafabesnili@hotmail.com>
2014-10-14 19:52:03 +0300

    initial commit

---
+++ iOS/Promoskop/Promoskop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Promoskop
+//
+//  Created by Mustafa Besnili on 14/10/14.
+//  Copyright (c) 2014 Mustafa Besnili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
