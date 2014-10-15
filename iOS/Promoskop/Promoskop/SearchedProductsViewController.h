943fbe36802d222673b8896b7127627dffb86db5
Mustafa Besnili <mustafabesnili@hotmail.com>
2014-10-15 20:50:00 +0300

    searchProduct implemented, copy db to device

---
+++ iOS/Promoskop/Promoskop/SearchedProductsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchedProductsViewController.h
+//  Promoskop
+//
+//  Created by Mustafa Besnili on 15/10/14.
+//  Copyright (c) 2014 Mustafa Besnili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchedProductsViewController : UIViewController
+@property (nonatomic, strong) NSArray *foundProducts;
+@end
