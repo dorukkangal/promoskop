18c8c970d0b9253276ea354719b3c759a393c3ff
dorukkangal <dorukkangal@gmail.com>
2014-10-22 03:53:39 +0300

    Json icerisindeki ters slash isaretleri kaldirildi

--- Promoskop-BackEnd/src/main/java/com/mudo/promoskop/web/controller/ProductController.java
+++ Promoskop-BackEnd/src/main/java/com/mudo/promoskop/web/controller/ProductController.java
@@ -3,13 +3,12 @@ package com.mudo.promoskop.web.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mudo.promoskop.web.model.Product;
 import com.mudo.promoskop.web.util.AppContext;
@@ -19,26 +18,27 @@ import com.mudo.promoskop.web.util.JsonPropertyFilter;
 @RequestMapping(value = "/product")
 public class ProductController {
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody
-	String getProductByIdInJSON(@PathVariable(value = "id") int id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ModelAndView getProductByIdInJSON(@PathVariable(value = "id") int id) {
 
 		Product product = AppContext.getProductService().find(id);
 		try {
-			return JsonPropertyFilter.generateJson(new String[] {"url"}, Arrays.asList(product));
+			ModelAndView model = new ModelAndView("json");
+			return model.addObject("json", JsonPropertyFilter.generateJson(new String[] { "url" }, Arrays.asList(product)));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 
-	@RequestMapping(value = "/findBySubString", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody
-	String getProductByNameInJSON(@RequestParam(value = "text") String text) {
+	@RequestMapping(value = "/findBySubString", method = RequestMethod.GET)
+	public ModelAndView getProductByNameInJSON(@RequestParam(value = "text") String text) {
 
 		List<Product> matchingProductList = AppContext.getProductService().findBySubString(text);
 		try {
-			return JsonPropertyFilter.generateJson(new String[] { "price", "branch_name", "address", "latitude", "longtitude", "store_name" }, matchingProductList);
+			ModelAndView model = new ModelAndView("json");
+			return model.addObject("json",
+					JsonPropertyFilter.generateJson(new String[] { "price", "branch_name", "address", "latitude", "longtitude", "store_name" }, matchingProductList));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
---
+++ Promoskop-BackEnd/src/main/webapp/WEB-INF/pages/json.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<style type="text/css"></style>
+</head>
+<body>
+	<pre style="word-wrap: break-word; white-space: pre-wrap;">${json}</pre>
+</body>
+</html>
